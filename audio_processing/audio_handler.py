062c0e287abbf7e4f07b6c11b17f3b8f383b50b4
Thomas Fisher <tfisher013@gmail.com>
2023-12-30 15:22:17 -0700

    Speech to text is somewhat functional

--- audio_processing/audio_handler.py
+++ audio_processing/audio_handler.py
@@ -6,19 +6,16 @@ import matplotlib.pyplot as plt
 import speech_recognition as sr
 import sys
 from pydub import AudioSegment
-
 import pyaudio
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 1 #if sys.platform == 'darwin' else 2
+CHANNELS = 1 if sys.platform == 'darwin' else 2
 RATE = 44100
 RECORD_SECONDS = 5
 
 ambient_noise_level = 0
-
 recording_file_path = 'command_recording_file.wav'
-
 end_partition = 7
 r = sr.Recognizer()
 
@@ -129,23 +126,40 @@ def start_speech_to_text():
                     print('Ambient noise level: ', ambient_noise_level)
                     break
 
+                # experimentally determined sound thresholds; threshold for identifying the start of
+                # an utterance needs to be higher than that denoting the end of one
+                start_speech_threshold = 1.25 * ambient_noise_level
+                end_speech_threshold = 1.05 * ambient_noise_level
+                silence_threshold = 1.5 * ambient_noise_level
+
                 max_val = max(vol_list)
                 min_val = min(vol_list)
-                silence_threshold = 1.25 * ambient_noise_level
                 # case 1: audio looks to contain sound
-                if max_val >= 1.5 * ambient_noise_level:
+                if max_val >= silence_threshold:
 
                     print('Detected sound in file, silence threshold is ', silence_threshold, ' for min: ', min_val, ', max: ', max_val)
 
-                    # case 2: sound within sample is isolated --> BEGIN BY TESTING THIS CASE
+                    # check whether sound in file is isolated (extends to to end of file or not)
+                    is_sound_isolated = True
                     end_check_index = round((actual_samples * (1.0 - (1.0 / end_partition))))
-                    end_check_len = round((actual_samples * (1.0 / end_partition)))
-                    print('Average of last fifth of values is ', sum(vol_list[end_check_index:]) / end_check_len, ', given by index ', end_check_index)
-                    if sum(vol_list[end_check_index:]) / end_check_len <= 1.05 * silence_threshold:      
+                    num_matches = 0
+                    for i in range(end_check_index, actual_samples):
+                        if vol_list[i] > start_speech_threshold:
+                            num_matches += 1
+
+                            # if we find a certain number of samples above a certain threshold
+                            # in the last 1/end_partition samples, consider the sound to be not
+                            # isolated
+                            if num_matches > 3:
+                                is_sound_isolated = False
+                                break
+
+                    # case 2: sound within sample is isolated -> convert to text
+                    if is_sound_isolated:      
                         print('--> Sound is isolated')
                         convert_audio = True
 
-                        # could we parallelize the two checks with threads below?
+                        # could we parallelize the two checks below with threads?
 
                         start_silence_index = 0
                         end_silence_index = num_samples
@@ -153,7 +167,7 @@ def start_speech_to_text():
                         # find starting index of sound from the start
                         count = 0
                         for i in range(0, num_samples):
-                            if vol_list[i] >= silence_threshold:
+                            if vol_list[i] >= start_speech_threshold:
                                 count += 1
                                 if count == sound_start_threshold:
                                     start_silence_index = max(0, i - 2 * count)
@@ -161,7 +175,7 @@ def start_speech_to_text():
                         # find starting index of sound from the end
                         count = 0
                         for i in reversed(range(0, num_samples)):
-                            if vol_list[i] >= 1.05 * ambient_noise_level:
+                            if vol_list[i] >= end_speech_threshold:
                                 count += 1
                                 if count == sound_start_threshold:
                                     end_silence_index = min(i + 6 * count, actual_samples)
